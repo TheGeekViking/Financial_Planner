c75822109395721cfeb26af68aa74fa34de407dd
The Geek Viking <86496812+TheGeekViking@users.noreply.github.com>
2022-04-25 15:44:08 -0400

    Initial Commit

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,35 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+// VLOG
+
+let listVideo = document.querySelectorAll('.video-list .vid');
+let mainVideo = document.querySelector('.main-video video');
+let title = document.querySelector('.main-video .title');
+
+listVideo.forEach(video => {
+    video.onclick = () => {
+        listVideo.forEach(vid => vid.classList.remove('active'));
+        video.classList.add('active');
+        if(video.classList.contains('active')) {
+            let src = video.children[0].getAttribute('src');
+            mainVideo.src = src;
+            let text = video.children[1].innerHTML;
+            title.innerHTML = text;
+        };
+    };
+});
+
+// Pop Up Video Intro
+
+const btn = document.querySelector('.btn');
+const videoContainer = document.querySelector('.intro-video-container');
+const close = document.querySelector('.video-intro-close');
+
+btn.addEventListener('click', () => {
+    videoContainer.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    videoContainer.classList.remove('show');
+});
